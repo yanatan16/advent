49de57fdc83129c55cb74c6128e816bbafb8078d
Jon Eisen <jeisen@apple.com>
2021-12-06 09:00:28 -0700

    day 6

--- 2021/AdventOfCode2021.playground/Pages/Day6.xcplaygroundpage/Contents.swift
+++ 2021/AdventOfCode2021.playground/Pages/Day6.xcplaygroundpage/Contents.swift
@@ -3,20 +3,20 @@
 import Foundation
 
 let exampleRaw = """
-example
+3,4,3,1,2
 """
 
 let example = parseInput(exampleRaw)
-print("Input Example: \(example)")
+print("Input Example: \(example.ages)")
 
 let input = parseInput(try! readInput())
 
-print("Example Solution 1: \(problem1(example))")
+print("Example Solution 1: \(Day6.problem1(example))")
 
-print("Real Solution 1: \(problem1(input))")
+print("Real Solution 1: \(Day6.problem1(input))")
 
-print("Example Solution 2: \(problem2(example))")
+print("Example Solution 2: \(Day6.problem2(example))")
 
-print("Real Solution 2: \(problem2(input))")
+print("Real Solution 2: \(Day6.problem2(input))")
 
 //: [Next](@next)
--- 2021/AdventOfCode2021.playground/Pages/Day6.xcplaygroundpage/Sources/Day6Impl.swift
+++ 2021/AdventOfCode2021.playground/Pages/Day6.xcplaygroundpage/Sources/Day6Impl.swift
@@ -1,18 +1,64 @@
 import Foundation
 
 public struct Input {
-    
+    public let ages : [Int]
 }
 public typealias Output = Int
 
 public func parseInput(_ raw: String) -> Input {
-    Input()
+    Input(
+        ages: raw.components(separatedBy: "\n")
+            .first!
+            .components(separatedBy: ",")
+            .compactMap { Int($0) }
+    )
 }
 
-public func problem1(_ input: Input) -> Output {
-    -1
+struct State {
+    let ageCounts : [Int:Int]
+    
+    func ageBy1Day() -> State {
+        State(ageCounts: [
+            0:ageCounts[1, default:0],
+            1:ageCounts[2, default:0],
+            2:ageCounts[3, default:0],
+            3:ageCounts[4, default:0],
+            4:ageCounts[5, default:0],
+            5:ageCounts[6, default:0],
+            6:ageCounts[7, default:0] + ageCounts[0, default:0],
+            7:ageCounts[8, default:0],
+            8:ageCounts[0, default:0]
+        ])
+    }
+    
+    var total : Int {
+        ageCounts.values.reduce(0) { $0 + $1 }
+    }
 }
 
-public func problem2(_ input: Input) -> Output {
-    -1
+public enum Day6 {
+    public static func problem1(_ input: Input) -> Output {
+        var state = State(ageCounts: input.ages.reduce(into: [:]) { (cnts, age) in
+            cnts[age, default:0] += 1
+        })
+        
+        (1...80).forEach { _ in
+            state = state.ageBy1Day()
+        }
+        
+        return state.total
+    }
+    
+    public static func problem2(_ input: Input) -> Output {
+        var state = State(ageCounts: input.ages.reduce(into: [:]) { (cnts, age) in
+            cnts[age, default:0] += 1
+        })
+        
+        (1...256).forEach { _ in
+            state = state.ageBy1Day()
+        }
+        
+        return state.total
+    }
+    
 }
