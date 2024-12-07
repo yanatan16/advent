2884c3d8b013a2b0b46c6df588260beb59e852a5
Jon Eisen <jon@joneisen.me>
2024-12-06 22:52:08 -0700

    smart day 7

--- 2024/solutions/day7.py
+++ 2024/solutions/day7.py
@@ -40,7 +40,7 @@ class Day7(Advent[Input]):
 
     def could_be_true_p1(self, case: Tuple[int, List[int]]) -> bool:
       test, values = case
-      
+
       for ops in self.operations(len(values) - 1):
         total = values[0]
 
@@ -53,12 +53,28 @@ class Day7(Advent[Input]):
         if total == test:
           self._print('true', test, ops, values)
           return True
-      
+
       return False
+
+    def could_be_true_p1_smart(self, case: Tuple[int, List[int]]) -> bool:
+      test, values = case
+
+      if len(values) == 1:
+        return test == values[0]
+      elif test < values[0]:
+        return False
+      else:
+        # + is always an option
+        if self.could_be_true_p1_smart((test - values[-1], values[:-1])):
+          return True
+        elif test % values[-1] == 0: # * is only an option is last value divides test
+          return self.could_be_true_p1_smart((test // values[-1], values[:-1]))
+        else:
+          return False
 
     def solve1(self, input: Input) -> Any:
       return sum(
-        case[0] if self.could_be_true_p1(case) else 0
+        case[0] if self.could_be_true_p1_smart(case) else 0
         for case in input
       )
 
@@ -71,7 +87,7 @@ class Day7(Advent[Input]):
 
     def could_be_true_p2(self, case: Tuple[int, List[int]]) -> bool:
       test, values = case
-      
+
       for ops in self.operations_p2(len(values) - 1):
         total = values[0]
 
@@ -86,14 +102,37 @@ class Day7(Advent[Input]):
         if total == test:
           self._print('true', test, ops, values)
           return True
-      
+
       return False
+
+
+    def could_be_true_p2_smart(self, case: Tuple[int, List[int]]) -> bool:
+      test, values = case
+
+      if len(values) == 1:
+        return test == values[0]
+      elif test < values[0]:
+        return False
+      else:
+        # + is always an option
+        v = values[-1]
+        if self.could_be_true_p2_smart((test - v, values[:-1])):
+          return True
+        # * is only an option is last value divides test
+        elif test % v == 0 and self.could_be_true_p2_smart((test // v, values[:-1])):
+          return True
+        elif str(test).endswith(str(v)):
+          reduced = int(str(test)[:-(len(str(v)))])
+          return self.could_be_true_p2_smart((reduced, values[:-1]))
+        else:
+          return False
 
     def solve2(self, input: Input) -> Any:
       return sum(
-        case[0] if self.could_be_true_p2(case) else 0
+        case[0] if self.could_be_true_p2_smart(case) else 0
         for case in tqdm(input)
       )
 
+    
 if __name__ == '__main__':
     Day7().main()
